af9209f0f9dc3cad3d611efc8394fd8cd44760dc
michael thomas <michael.thomas2014@gmail.com>
2020-05-09 04:33:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(word)
   new_array = word.split(" ")
-  new_string = ""
+  new_string = []
  new_array.each do |words|
-   new_string += words.reverse
+   new_string << words.reverse
  end
- new_string
+ new_string.join(" ")
 end
